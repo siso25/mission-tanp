55505c5ff7920af521b3a42ca2dfec4c8255054a
siba <siso25@zoho.com>
2020-05-17 17:30:32 +0900

    ヘッダーとフッターの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root 'users#index'
-  resources :users
+  root 'gifts#index'
+  # resources :users
+  resources :gifts, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
