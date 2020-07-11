8ab92a80937dd2b1212588985dcc6889e738f8bd
siso <siso25@zoho.com>
2020-07-11 21:59:03 +0900

    トップページのバックエンド実装完了（パフォーマンス以外）

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -1,11 +1,13 @@
 class GiftsController < ApplicationController
 
   def index
-    @ranking_orders = RankingOrder.where(category_id: 1).order(:rank)
-    first_rank_product = @ranking_orders.select { |ranking_order| ranking_order.rank == 1 }
-    @title = first_rank_product[0].category_name
+    @ranking_orders = RankingOrder.all.order(:category_id, :rank)
+    @ranking_categories = RankingOrder.select(:category_id, :category_name).distinct.where.not(category_id: 5).order(:category_id)
+    @new_product_rankings = @ranking_orders.select { |ranking_order| ranking_order.category_id == 5 }
   end
 
   def show
+    @gift = Gift.find(params[:id])
+    @shipping_date = (Date.today + 3).strftime("%-m月%-d日")
   end
 end
