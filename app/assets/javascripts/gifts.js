b271668b799181e7c7c8156429e64b0368fdc71e
siso <siso25@zoho.com>
2020-07-14 22:10:31 +0900

    モーダルウインドウの実装

--- app/assets/javascripts/gifts.js
+++ app/assets/javascripts/gifts.js
@@ -1,3 +1,8 @@
 $(document).on('turbolinks:load', function () {
-    
+    $('.js-modal-open').on('click', function() {
+        $('.modal-wrapper').fadeIn();
+    });
+    $('.js-modal-close').on('click', function() {
+        $('.modal-wrapper').fadeOut();
+    });
 });
