06db8dac4c9648a2e5fdb12f12b4d00a9307ab50
siso <siso25@zoho.com>
2020-08-10 23:21:35 +0900

    ログイン有無による画面の切り替え

--- app/assets/javascripts/gifts.js
+++ app/assets/javascripts/gifts.js
@@ -5,4 +5,25 @@ $(document).on('turbolinks:load', function () {
     $('.js-modal-close').on('click', function() {
         $('.modal-wrapper').fadeOut();
     });
+
+    $('.js-header-dropdown-open').on('click', function() {
+        const existsIsHidden = $('.js-header-dropdown-menu').hasClass('login-nav-hidden');
+        if (existsIsHidden) {
+            $('.js-header-dropdown-menu').removeClass('login-nav-hidden')
+            $('.js-header-dropdown-menu').addClass('login-nav');
+        } else {
+            $('.js-header-dropdown-menu').addClass('login-nav-hidden')
+            $('.js-header-dropdown-menu').removeClass('login-nav');
+        }
+    });
+
+    $(document).on('click', function (event) {
+        if (!$(event.target).closest('.js-header-dropdown-menu').length && !$(event.target).closest('.js-header-dropdown-open').length)  {
+            const existsIsHidden = $('.js-header-dropdown-menu').hasClass('login-nav-hidden');
+            if (!existsIsHidden) {
+                $('.js-header-dropdown-menu').addClass('login-nav-hidden')
+                $('.js-header-dropdown-menu').removeClass('login-nav');
+            }
+        }
+    });
 });
