41d6f7254b2ac3441af30703194805baa8b32a54
siso <siso25@zoho.com>
2020-08-25 22:03:34 +0900

    記念日の月日だけでソートして一覧表示できるように実装

--- app/controllers/preciouspeople_controller.rb
+++ app/controllers/preciouspeople_controller.rb
@@ -1,5 +1,8 @@
 class PreciouspeopleController < ApplicationController
   def index
+    @precious_people = PreciousPerson.where(user_id: current_user.id)
+    @precious_days = PreciousDay.includes(:precious_person).where('precious_people.user_id = ?', current_user.id)
+      .references(:precious_people).order(Arel.sql("to_char(precious_days.precious_date, 'MMDD')"))
   end
 
   def show
